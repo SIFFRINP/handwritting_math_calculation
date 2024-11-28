f8c7edd69c69b58291b5a1ce2585a1bb482cf421
romainflcht <romain.flachat@gmail.com>
2024-11-28 15:29:20 +0000

    v1.1: Added source for UI

---
+++ configuration.py
@@ -0,0 +1,10 @@
+
+# * _ WINDOW ___________________________________________________________________
+WINDOW_SIZE = (500, 300) # WIDTH x HEIGHT
+
+# * _ COLORS ___________________________________________________________________
+BACKGROUND_COLOR = (255, 255, 255)
+PEN_COLOR        = (0  , 0  , 0  )
+
+if __name__ == "__main__": 
+    print("\x1b[33m~[WARNING] This script is not meant to be executed.\x1b[0m"); 
--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello, World!")
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
+import pygame
+from configuration import * 
+
+
+if __name__ == "__main__":
+
+    # * _ WINDOW INIT __________________________________________________________
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+    pygame.display.set_caption('handwritting_math_calculation')
+
+
+    running = True
+    while running:
+
+    # * _ MAIN LOOP ____________________________________________________________
+        x, y = pygame.mouse.get_pos()
+        
+        screen.fill(BACKGROUND_COLOR)
+        pygame.draw.circle(screen, PEN_COLOR, (x, y), 5)
+        pygame.display.flip()
+
+
+    # * _ EVENT HANDLER ________________________________________________________
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
